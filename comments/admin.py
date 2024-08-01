66356ba927f34397439b3508ccc0497380188452
Serhii Berdianskyi <zeferot94@gmail.com>
2024-08-01 11:20:16 +0300

    changed Commentary model name to Comment; redefined str methods for Post and Comment models; added models to admin.py

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from comments.models import User, Post, Comment
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "homepage"]
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["owner", "text"]
+
+
+@admin.register(Comment)
+class CommentaryAdmin(admin.ModelAdmin):
+    list_display = ["owner", "text", "post"]
--- comments/models.py
+++ comments/models.py
@@ -4,7 +4,10 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=255, blank=False, null=False)
     email = models.EmailField(blank=False, null=False)
-    homepage = models.URLField()
+    homepage = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.email})"
 
 
 class Post(models.Model):
@@ -16,10 +19,10 @@ class Post(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{User.username} posted at {self.created_time}"
+        return f"{self.owner.username} posted at {self.created_time}"
 
 
-class Commentary(models.Model):
+class Comment(models.Model):
     owner = models.ForeignKey(User,
                               on_delete=models.CASCADE,
                               related_name="comments"
@@ -31,4 +34,4 @@ class Commentary(models.Model):
                              related_name="posts")
 
     def __str__(self):
-        return f"{User.username}: {self.text}"
+        return f"{self.owner.username}: {self.text}"
