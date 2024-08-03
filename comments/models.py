0d589145015a3e6dac3b37becc2e3706b39dfad0
Serhii Berdianskyi <zeferot94@gmail.com>
2024-08-03 17:08:14 +0300

    changed project model

--- comments/models.py
+++ comments/models.py
@@ -1,37 +1,12 @@
 from django.db import models
 
 
-class User(models.Model):
+class Comment(models.Model):
     username = models.CharField(max_length=255, blank=False, null=False)
     email = models.EmailField(blank=False, null=False)
     homepage = models.URLField(blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.username} ({self.email})"
-
-
-class Post(models.Model):
-    owner = models.ForeignKey(User,
-                              on_delete=models.CASCADE,
-                              related_name="posts"
-                              )
-    text = models.CharField(max_length=255, blank=False, null=False)
+    text = models.TextField(blank=False, null=False)
     created_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.owner.username} posted at {self.created_time}"
-
-
-class Comment(models.Model):
-    owner = models.ForeignKey(User,
-                              on_delete=models.CASCADE,
-                              related_name="comments"
-                              )
-    text = models.CharField(max_length=255)
-    created_time = models.DateTimeField(auto_now_add=True)
-    post = models.ForeignKey(Post,
-                             on_delete=models.CASCADE,
-                             related_name="posts")
-
-    def __str__(self):
-        return f"{self.owner.username}: {self.text}"
+        return f"{self.username}: {self.text}"
