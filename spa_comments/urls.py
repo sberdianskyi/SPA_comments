31de071de207f492b6f4a19bd4f8b77a24594e20
Serhii Berdianskyi <zeferot94@gmail.com>
2024-08-03 17:11:42 +0300

    adde path in root urls.py

--- spa_comments/urls.py
+++ spa_comments/urls.py
@@ -14,3 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("captcha/", include("captcha.urls")),
+    path("", include("comments.urls", namespace="comments")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
