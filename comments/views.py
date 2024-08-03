10011bc708e8b41118f949656375c12ff2a1b263
Serhii Berdianskyi <zeferot94@gmail.com>
2024-08-03 17:15:12 +0300

    added views in comments/views.py

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from comments.forms import CommentForm
+from comments.models import Comment
+
+
+class CommentsListView(generic.ListView):
+    model = Comment
+    template_name = "comments/index.html"
+    context_object_name = "comment_list"
+    ordering = ["-created_time"]
+    paginate_by = 25
+
+
+class CommentsCreateView(generic.CreateView):
+    model = Comment
+    form_class = CommentForm
+    success_url = reverse_lazy("comments:comment-list")
+    template_name = "comments/comment_form.html"
