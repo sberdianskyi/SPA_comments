92e4748c99d6c3785f29516d95428a913f595d16
Serhii Berdianskyi <zeferot94@gmail.com>
2024-08-14 10:19:36 +0300

    added ReplyCreateView

--- comments/views.py
+++ comments/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views import generic
 
-from comments.forms import CommentForm
+from comments.forms import CommentForm, ReplyForm
 from comments.models import Comment
 
 
@@ -15,7 +14,19 @@ class CommentsListView(generic.ListView):
 
 
 class CommentsCreateView(generic.CreateView):
-    model = Comment
     form_class = CommentForm
     success_url = reverse_lazy("comments:comment-list")
     template_name = "comments/comment_form.html"
+
+
+class ReplyCreateView(generic.CreateView):
+    form_class = ReplyForm
+    success_url = reverse_lazy("comments:comment-list")
+    template_name = "comments/reply_form.html"
+
+    def form_valid(self, form):
+        comment_id = self.kwargs['comment_id']
+        comment = Comment.objects.get(id=comment_id)
+        form.instance.comment = comment
+
+        return super().form_valid(form)
